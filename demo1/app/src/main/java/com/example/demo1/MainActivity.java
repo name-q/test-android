71c11eb357b25368d7984730fd4aaa1e17281bc6
name-q <1620206666@qq.com>
2025-11-18 14:08:54 +0800

    activity

--- demo1/app/src/main/java/com/example/demo1/MainActivity.java
+++ demo1/app/src/main/java/com/example/demo1/MainActivity.java
@@ -13,8 +13,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Start drawing from the StatusBar
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //safe view
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
