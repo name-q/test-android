e9bc912524266ed871d1912daaff713d5bf53ed3
name-q <1620206666@qq.com>
2025-11-27 16:29:04 +0800

    sign up page

--- demo1/app/src/main/java/com/example/demo1/MainActivity.java
+++ demo1/app/src/main/java/com/example/demo1/MainActivity.java
@@ -16,26 +16,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
+         setContentView(R.layout.sign_up_linear);
 
-        // 线形布局
-        LinearLayout ll = new LinearLayout(this);
-        // 设置宽高 继承父 等宽等高
-        ll.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
-        // 背景为红色
-        ll.setBackgroundColor(Color.RED);
-
-        setContentView(ll);
+//        // 线形布局
+//        LinearLayout ll = new LinearLayout(this);
+//        // 设置宽高 继承父 等宽等高
+//        ll.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+//        // 背景为红色
+//        ll.setBackgroundColor(Color.RED);
+//
+//        setContentView(ll);
 
 
         //Start drawing from the StatusBar
         EdgeToEdge.enable(this);
         //safe view
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.sign), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
 
     }
